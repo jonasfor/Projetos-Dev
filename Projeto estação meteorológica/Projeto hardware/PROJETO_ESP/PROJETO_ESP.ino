5236041ed7e6441456c0c42aafce805638b5444d
jonasfor <55710392+jonasfor@users.noreply.github.com>
2019-10-10 21:12:33 -0300

    Add files via upload

---
+++ Projeto estação meteorológica/Projeto hardware/PROJETO_ESP/PROJETO_ESP.ino
@@ -0,0 +1,34 @@
+'#include <ETH.h>
+#include <WiFi.h>
+#include <WiFiAP.h>
+#include <WiFiClient.h>
+#include <WiFiGeneric.h>
+#include <WiFiMulti.h>
+#include <WiFiScan.h>
+#include <WiFiServer.h>
+#include <WiFiSTA.h>
+#include <WiFiType.h>
+#include <WiFiUdp.h>
+
+const char* ssid = "Internet";
+const char* password =  "fordcs2607";
+ 
+void setup() {
+ 
+  Serial.begin(115200);
+ 
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Conectando WiFi..");
+  }
+ 
+  Serial.println("Conectado a rede WiFi");
+ 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
